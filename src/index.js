c604e816b3ac89650bca9fdd53eea32c66ceca69
Mark Tsoy <tsoy.mark@MBA15.local>
2025-04-23 00:20:14 +0500

    restore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import "./styles/index.css";
-import { Header } from "./components/Header/Header";
-import { AboutProject } from "./components/AboutProject/AboutProject";
-import { Speakers } from "./components/Speakers/Speakers";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const content = document.createElement("main");
-  content.className = "content";
-  document.body.appendChild(content);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  new Header();
-  new AboutProject();
-  new Speakers();
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
+export default {
+  root: "src",
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     emptyOutDir: true,
   },
   server: {
     port: 3000,
     open: true,
   },
-  publicDir: "public",
-  base: "/",
-});
+};
